aa6046763969a06c3603ba017480f6cd667f3de1
malindamahanani <92916297+malindamahanani@users.noreply.github.com>
2022-11-17 15:02:28 +0700

    Update App.js
    update

--- App.js
+++ App.js
@@ -1,40 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { Card } from "react-native-paper";
-import { Text,View,FlatList, ImageBackground} from 'react-native';
-import styles from './styles';
-
-export default function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://raw.githubusercontent.com/algosigma/js-reactjs/master/homestays.json')
-    .then((response)=> response.json())
-    .then((hasil)=> setData(hasil))
-    .catch(error=> {console.log})
-  }, []);
-
-  const listHomestay = ({item}) => {
-    return (
-      <Card style={styles.cardUtama}>
-        <View style={styles.containerTitle}>
-          <Text style={styles.title}>{item.nama}</Text>
-        </View>
-        <View style={styles.containerImage}>
-          <ImageBackground style={styles.image}
-          source={{uri:item.fotoUrl}}>
-            <Text style={styles.price}>Rp. {item.harga} Rb</Text>
-          </ImageBackground>
-        </View>
-      </Card>
-    )
+ import * as React from 'react';
+ import { View, Text } from 'react-native';
+ import { NavigationContainer} from '@react-navigation/native';
+ import { createNativeStackNavigator} from '@react-navigation/native-stack';
+ import Home from './navigasi/Home';
+ import Detail from './navigasi/Detail';
+ 
+ const stack = createNativeStackNavigator();
+ 
+ 
+  function App() {
+   return (
+     <NavigationContainer>
+       <stack.Navigator>
+         <stack.Screen name='Home' component={Home}
+         options={{
+           title: 'Data Karyawan',
+           headerStyle: { backgroundColor: '#006aff'},
+           headerTintColor: '#fff'
+         }} />
+         <stack.Screen name='Detail' component={Detail}
+         options={{
+           title: 'Detail Karyawan',
+           headerStyle: { backgroundColor: '#006aff' },
+           headerTintColor: '#fff'
+         }}/>
+       </stack.Navigator>
+     </NavigationContainer>
+   )
+   
   }
-
-  return (
- <View style={{flex:1, padding:24}}>
-    <FlatList
-    data={data}
-    keyExtractor={item=>item.id}
-    renderItem={listHomestay} />
- </View>
-  );
-}
+ 
+ export default App;
+ 
